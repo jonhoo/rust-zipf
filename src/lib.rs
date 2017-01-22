02158cb5bb08f9db51c07070089f25800a9a9045
Jon Gjengset <jon@thesquareplanet.com>
2017-01-22 16:18:53 -0500

    Initial port

---
+++ src/lib.rs
@@ -0,0 +1,238 @@
+//! A fast generator of discrete, bounded
+//! [Zipf-distributed](https://en.wikipedia.org/wiki/Zipf's_law) random numbers.
+//!
+//! For a random variable `X` whose values are distributed according to this distribution, the
+//! probability mass function is given by
+//!
+//! ```ignore
+//! P(X = k) = H(N,s) * 1 / k^s for k = 1,2,...,N
+//! ```
+//!
+//! `H(N,s)` is the normalizing constant which corresponds to the generalized harmonic number
+//! of order `N` of `s`.
+//!
+//!
+//! This implementation is effectively a direct port of Apache Common's
+//! [RejectionInversionZipfSampler](https://github.com/apache/commons-rng/blob/6a1b0c16090912e8fc5de2c1fb5bd8490ac14699/commons-rng-sampling/src/main/java/org/apache/commons/rng/sampling/distribution/RejectionInversionZipfSampler.java),
+//! written in Java. It is based on the method described by Wolfgang Hörmann and Gerhard Derflinger
+//! in [*Rejection-inversion to generate variates from monotone discrete
+//! distributions*](https://dl.acm.org/citation.cfm?id=235029) from *ACM Transactions on Modeling
+//! and Computer Simulation (TOMACS) 6.3 (1996)*.
+
+extern crate rand;
+use rand::Rng;
+
+/// Random number generator that generates Zipf-distributed random numbers using rejection
+/// inversion.
+pub struct ZipfDistribution<R> {
+    /// Number of elements
+    num_elements: isize,
+    /// Exponent parameter of the distribution
+    exponent: f64,
+    /// `hIntegral(1.5) - 1}`
+    h_integral_x1: Option<f64>,
+    /// `hIntegral(num_elements + 0.5)}`
+    h_integral_num_elements: Option<f64>,
+    /// `2 - hIntegralInverse(hIntegral(2.5) - h(2)}`
+    s: Option<f64>,
+    /// Feeding random number generator
+    sampler: R,
+}
+
+impl<R: Rng> ZipfDistribution<R> {
+    /// Creates a new [Zipf-distributed](https://en.wikipedia.org/wiki/Zipf's_law)
+    /// random number generator.
+    ///
+    /// Note that both the number of elements and the exponent must be greater than 0.
+    pub fn new(sampler: R, num_elements: usize, exponent: f64) -> Result<Self, ()> {
+        if num_elements == 0 {
+            return Err(());
+        }
+        if exponent <= 0f64 {
+            return Err(());
+        }
+
+        let mut z = ZipfDistribution {
+            num_elements: num_elements as isize,
+            exponent: exponent,
+            h_integral_x1: None,
+            h_integral_num_elements: None,
+            s: None,
+            sampler: sampler,
+        };
+
+        // populate cache
+        let h_integral_x1 = z.h_integral(1.5) - 1f64;
+        z.h_integral_x1 = Some(h_integral_x1);
+        let h_integral_num_elements = z.h_integral(num_elements as f64 + 0.5);
+        z.h_integral_num_elements = Some(h_integral_num_elements);
+        let s = 2f64 - z.h_integral_inv(z.h_integral(2.5) - z.h(2f64));
+        z.s = Some(s);
+
+        Ok(z)
+    }
+}
+
+impl<R: Rng> Rng for ZipfDistribution<R> {
+    fn next_u32(&mut self) -> u32 {
+        // The paper describes an algorithm for exponents larger than 1 (Algorithm ZRI).
+        //
+        // The original method uses
+        //   H(x) = (v + x)^(1 - q) / (1 - q)
+        // as the integral of the hat function.
+        //
+        // This function is undefined for q = 1, which is the reason for the limitation of the
+        // exponent.
+        //
+        // If instead the integral function
+        //   H(x) = ((v + x)^(1 - q) - 1) / (1 - q)
+        // is used, for which a meaningful limit exists for q = 1, the method works for all
+        // positive exponents.
+        //
+        // The following implementation uses v = 0 and generates integral number in the range [1,
+        // num_elements]. This is different to the original method where v is defined to
+        // be positive and numbers are taken from [0, i_max]. This explains why the implementation
+        // looks slightly different.
+
+        // We know these were computed in new()
+        let hnum = self.h_integral_num_elements.unwrap();
+        let h_x1 = self.h_integral_x1.unwrap();
+        let s = self.s.unwrap();
+
+        loop {
+            let u: f64 = hnum + self.sampler.next_f64() * (h_x1 - hnum);
+            // u is uniformly distributed in (h_integral_x1, h_integral_num_elements]
+
+            let x: f64 = self.h_integral_inv(u);
+            let mut k: isize = (x + 0.5) as isize;
+
+            // Limit k to the range [1, num_elements] if it would be outside
+            // due to numerical inaccuracies.
+            if k < 1 {
+                k = 1;
+            } else if k > self.num_elements {
+                k = self.num_elements;
+            }
+
+            // Here, the distribution of k is given by:
+            //
+            //   P(k = 1) = C * (hIntegral(1.5) - h_integral_x1) = C
+            //   P(k = m) = C * (hIntegral(m + 1/2) - hIntegral(m - 1/2)) for m >= 2
+            //
+            // where C = 1 / (h_integral_num_elements - h_integral_x1)
+
+            let k64 = k as f64;
+            if k64 - x <= s || u >= self.h_integral(k64 + 0.5) - self.h(k64) {
+
+                // Case k = 1:
+                //
+                //   The right inequality is always true, because replacing k by 1 gives
+                //   u >= hIntegral(1.5) - h(1) = h_integral_x1 and u is taken from
+                //   (h_integral_x1, h_integral_num_elements].
+                //
+                //   Therefore, the acceptance rate for k = 1 is P(accepted | k = 1) = 1
+                //   and the probability that 1 is returned as random value is
+                //   P(k = 1 and accepted) = P(accepted | k = 1) * P(k = 1) = C = C / 1^exponent
+                //
+                // Case k >= 2:
+                //
+                //   The left inequality (k - x <= s) is just a short cut
+                //   to avoid the more expensive evaluation of the right inequality
+                //   (u >= hIntegral(k + 0.5) - h(k)) in many cases.
+                //
+                //   If the left inequality is true, the right inequality is also true:
+                //     Theorem 2 in the paper is valid for all positive exponents, because
+                //     the requirements h'(x) = -exponent/x^(exponent + 1) < 0 and
+                //     (-1/hInverse'(x))'' = (1+1/exponent) * x^(1/exponent-1) >= 0
+                //     are both fulfilled.
+                //     Therefore, f(x) = x - hIntegralInverse(hIntegral(x + 0.5) - h(x))
+                //     is a non-decreasing function. If k - x <= s holds,
+                //     k - x <= s + f(k) - f(2) is obviously also true which is equivalent to
+                //     -x <= -hIntegralInverse(hIntegral(k + 0.5) - h(k)),
+                //     -hIntegralInverse(u) <= -hIntegralInverse(hIntegral(k + 0.5) - h(k)),
+                //     and finally u >= hIntegral(k + 0.5) - h(k).
+                //
+                //   Hence, the right inequality determines the acceptance rate:
+                //   P(accepted | k = m) = h(m) / (hIntegrated(m+1/2) - hIntegrated(m-1/2))
+                //   The probability that m is returned is given by
+                //   P(k = m and accepted) = P(accepted | k = m) * P(k = m) = C * h(m) = C / m^exponent.
+                //
+                // In both cases the probabilities are proportional to the probability mass function
+                // of the Zipf distribution.
+
+                return k as u32;
+            }
+        }
+    }
+}
+
+use std::fmt;
+impl<R: fmt::Debug> fmt::Debug for ZipfDistribution<R> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "Rejection inversion Zipf deviate [{:?}]", self.sampler)
+    }
+}
+
+impl<R: Rng> ZipfDistribution<R> {
+    /// Computes `H(x)`, defined as
+    ///
+    ///  - `(x^(1 - exponent) - 1) / (1 - exponent)`, if `exponent != 1`
+    ///  - `log(x)`, if `exponent == 1`
+    ///
+    /// `H(x)` is an integral function of `h(x)`, the derivative of `H(x)` is `h(x)`.
+    fn h_integral(&self, x: f64) -> f64 {
+        let log_x = x.ln();
+        helper2((1f64 - self.exponent) * log_x) * log_x
+    }
+
+    /// Computes `h(x) = 1 / x^exponent`
+    fn h(&self, x: f64) -> f64 {
+        (-self.exponent * x.ln()).exp()
+    }
+
+    /// The inverse function of `H(x)`.
+    /// Returns the `y` for which `H(y) = x`.
+    fn h_integral_inv(&self, x: f64) -> f64 {
+        let mut t: f64 = x * (1f64 - self.exponent);
+        if t < -1f64 {
+            // Limit value to the range [-1, +inf).
+            // t could be smaller than -1 in some rare cases due to numerical errors.
+            t = -1f64;
+        }
+        (helper1(t) * x).exp()
+    }
+}
+
+/// Helper function that calculates `log(1 + x) / x`.
+/// A Taylor series expansion is used, if x is close to 0.
+fn helper1(x: f64) -> f64 {
+    if x.abs() > 1e-8 {
+        x.ln_1p() / x
+    } else {
+        1f64 - x * (0.5 - x * (0.33333333333333333 - 0.25 * x))
+    }
+}
+
+/// Helper function to calculate `(exp(x) - 1) / x`.
+/// A Taylor series expansion is used, if x is close to 0.
+fn helper2(x: f64) -> f64 {
+    if x.abs() > 1e-8 {
+        x.exp_m1() / x
+    } else {
+        1f64 + x * 0.5 * (1f64 + x * 0.33333333333333333 * (1f64 + 0.25 * x))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn generate() {
+        use rand::{self, Rng};
+        use super::ZipfDistribution;
+        let rng = rand::thread_rng();
+        let mut g = ZipfDistribution::new(rng, 100000, 1.07).unwrap();
+        for _ in 0..100000 {
+            println!("{}", g.next_f64());
+        }
+    }
+}
